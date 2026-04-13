eacedfd651408873c8eb5c99ada17753f8e5110e
Lukasz Ozimek <lno91705@gmail.com>
2026-04-13 12:53:56 -0400

    updated user scipt

--- scripts/lukasz/user.ps1
+++ scripts/lukasz/user.ps1
@@ -1,46 +1,83 @@
-#This script will disable users not listed in the packet, create new users, and change passwords for all accounts listed in the packet
+# ============================================================
+# User Management & Password Rotation Script
+# - Disables users not in the approved list
+# - Creates new users
+# - Changes passwords for all listed accounts
+# - Rotates passwords every 10 minutes
+# ============================================================
 
 
-#List of users
-$users = @("zhukov", "gusev", "makarov", "festisov", "kasatonov", "krutov", "lebedev", "vasiliev", "tikhonov", "kulagin", "brezhnev", "tretiak", "larionov")
-$new_users = ("kaspirov", "gagarin", "korolev", "tupolev", "sukhoi", "yakovlev", "mig", "su", "tu", "il")
+# ---- User Lists ----
+$users     = @("zhukov", "gusev", "makarov", "festisov", "kasatonov", "krutov",
+               "lebedev", "vasiliev", "tikhonov", "kulagin", "brezhnev", "tretiak", "larionov")
+$new_users = @("kaspirov", "gagarin", "korolev", "tupolev", "sukhoi",
+               "yakovlev", "mig", "su", "tu", "il")
+$allUsers  = $users + $new_users
+$exemptUsers = $allUsers + @("grayteam", "greyteam", "Administrator", "scoring")
 
-#Create new users
-ForEach-Object {
-    foreach ($user in $new_users) {
-        if (-not (Get-LocalUser -Name $user -ErrorAction SilentlyContinue)) {
-            New-LocalUser -Name $user -Password (ConvertTo-SecureString "SuperSecure12345!" -AsPlainText -Force) -FullName $user -Description "Created by script"
-            Write-Host "Created user $user"
-        } else {
-            Write-Host "User $user already exists"
+
+# ---- Password Rotation List ----
+$passwordList = @(
+    "Volkov@Thunder91",
+    "Bering#Frost47",
+    "Kremlin!Watch33",
+    "Sputnik@Delta82",
+    "Taiga#Storm56",
+    "Polar!Vortex19",
+    "Siberia@Hawk74",
+    "Tundra#Blaze38",
+    "Arctic!Wolf65",
+    "Kazakh@Eagle21"
+)
+$passwordIndex = 0
+
+
+# ---- Create New Users ----
+$initialPassword = ConvertTo-SecureString $passwordList[0] -AsPlainText -Force
+
+foreach ($user in $new_users) {
+    if (-not (Get-LocalUser -Name $user -ErrorAction SilentlyContinue)) {
+        New-LocalUser -Name $user -Password $initialPassword -FullName $user -Description "Created by script"
+        Write-Host "Created user: $user"
+    } else {
+        Write-Host "User already exists: $user"
+    }
+}
+
+
+# ---- Disable Unlisted Users ----
+Get-LocalUser | ForEach-Object {
+    if ($_.Name -notin $exemptUsers) {
+        Disable-LocalUser -Name $_.Name
+        Write-Host "Disabled user: $($_.Name)"
+    }
+}
+
+
+# ---- Set Initial Passwords ----
+Get-LocalUser | ForEach-Object {
+    if ($_.Enabled -and ($_.Name -in $allUsers)) {
+        Set-LocalUser -Name $_.Name -Password $initialPassword
+        Write-Host "Set initial password for: $($_.Name)"
+    }
+}
+
+
+# ---- Password Rotation Loop (every 10 minutes) ----
+Write-Host "Starting password rotation loop..."
+
+while ($true) {
+    Start-Sleep -Seconds 600
+
+    $passwordIndex = ($passwordIndex + 1) % $passwordList.Count
+    $currentPassword = ConvertTo-SecureString $passwordList[$passwordIndex] -AsPlainText -Force
+
+    Write-Host "`n[$(Get-Date)] Rotating to password index $passwordIndex"
+
+    Get-LocalUser | ForEach-Object {
+        if ($_.Enabled -and ($_.Name -in $allUsers)) {
+            Set-LocalUser -Name $_.Name -Password $currentPassword
+            Write-Host "  Rotated password for: $($_.Name)"
         }
     }
 }
-
-
-#Change passwords
-$NewPassword = ConvertTo-SecureString "SuperSecure12345!" -AsPlainText -Force
-
-Get-LocalUser | ForEach-Object {
-    if ($_.Enabled -and $_.Name -in $users -or $_.Name -in $new_users) {
-        
-        Set-LocalUser -Name $_.Name -Password $NewPassword
-        
-        Write-Host "Updated password for $($_.Name)"
-    }
-}
-
-
-#Disable users not in the list
-Get-LocalUser | ForEach-Object {
-    if ($_.Name -notin @("zhukov", "gusev", "makarov", "festisov", "kasatonov", "krutov", "lebedev", "vasiliev", "tikhonov", "kulagin", "brezhnev", "tretiak", "larionov", "kaspirov", "gagarin", "korolev", "tupolev", "sukhoi", "yakovlev", "mig", "su", "tu", "il", "grayteam", "greyteam", "Administrator", "scoring")) {
-        Disable-LocalUser -Name $_.Name
-        Write-Host "Disabled user $($_.Name)"
-    }
-}
-
-
-
-#Make script rotate passwords every 5 minutes
-
-
